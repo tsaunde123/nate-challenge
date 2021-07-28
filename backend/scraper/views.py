0ce93ec02e10461d491014e31aec5473a36e2f93
TristanSaunders <tristansaundersfr@gmail.com>
2021-07-28 20:18:09 +0100

    add test view to return current time

--- backend/scraper/views.py
+++ backend/scraper/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
 @api_view(["GET"])
 def time(request):
-    return Response("success")
+    import time
+    return Response({"time": time.time()})
