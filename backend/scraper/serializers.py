0281942acf26184a120aad2b2d1a4a56ebdc35f4
TristanSaunders <tristansaundersfr@gmail.com>
2021-07-30 18:03:05 +0100

    backend: calculate total occurrences and max sample size in serializer

--- backend/scraper/serializers.py
+++ backend/scraper/serializers.py
@@ -9,6 +9,7 @@ class ScraperEntitySerializer(serializers.ModelSerializer):
     sample_size = serializers.IntegerField(required=True, write_only=True)
     completion_time = serializers.SerializerMethodField()
     word_occurrences = serializers.SerializerMethodField()
+    total_occurrences = serializers.SerializerMethodField()
 
     class Meta:
         model = ScraperEntity
@@ -17,16 +18,22 @@ class ScraperEntitySerializer(serializers.ModelSerializer):
             "word_occurrences",
             "sample_size",
             "completion_time",
+            "total_occurrences"
         )
 
     def get_completion_time(self, entity: ScraperEntity):
         return entity.end_time - entity.start_time
 
     def get_word_occurrences(self, entity: ScraperEntity):
+        max_sample_size = len(entity.word_occurrences)
         sample_size = self.context.get("sample_size", 10)
+        sample_size = max_sample_size if sample_size > max_sample_size else sample_size
         keys = list(entity.word_occurrences.keys())[:sample_size]
         sample = {key: entity.word_occurrences[key] for key in keys}
         return sample
+
+    def get_total_occurrences(self, entity: ScraperEntity):
+        return len(entity.word_occurrences)
 
     def create(self, validated_data):
         freeze_time = now()
