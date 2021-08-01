36018f01a2896ce043e8a49bd96a8d0a21617b14
TristanSaunders <tristansaundersfr@gmail.com>
2021-08-01 14:55:11 +0100

    back: add comment

--- backend/scraper/models.py
+++ backend/scraper/models.py
@@ -6,9 +6,10 @@ class ScraperEntity(models.Model):
     url = models.URLField(unique=True, db_index=True, blank=False, null=False)
     word_occurrences = models.JSONField(null=True, blank=True)
 
-    error = models.BooleanField(default=False)
     last_refresh = models.DateTimeField(auto_now_add=True)
 
+    # these properties can be moved to their own entity to track the status of scraper tasks
+    error = models.BooleanField(default=False)
     start_time = models.DateTimeField(auto_now_add=True)
     end_time = models.DateTimeField(null=True)
 
