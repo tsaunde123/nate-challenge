9a7844e520547bb8de010b3adb1e7b44b2ab9eb8
TristanSaunders <tristansaundersfr@gmail.com>
2021-08-02 22:46:56 +0100

    front: add test coverage reports

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   testEnvironment: "jsdom",
+  collectCoverage: true,
+  coverageDirectory: "<rootDir>/test/coverage",
   setupFilesAfterEnv: ["./jest.setup.js"],
   moduleNameMapper: {
     "^@/components(.*)$": "<rootDir>/components$1",
