8fcc4b749ac5a86d5d27f91c883bfcdb401bf28d
TristanSaunders <tristansaundersfr@gmail.com>
2021-08-02 16:07:45 +0100

    back: limited word count returned to 100 words

--- backend/scraper/views.py
+++ backend/scraper/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Prefetch
+from django.db.models import Prefetch, Subquery, OuterRef
 from rest_framework import mixins, viewsets, status
 from rest_framework.response import Response
 
@@ -21,6 +21,25 @@ class ScrapeViewSet(
             Prefetch("words", queryset=WordCount.objects.order_by("-count"))
         )
     )
+
+    def get_queryset(self):
+        # limit to n words to avoid slowing down the UI until pagination is implemented
+        word_count_limit = 100
+        subqry = Subquery(
+            WordCount.objects.filter(scrape_id=OuterRef("scrape_id")).values_list(
+                "id", flat=True
+            )[:word_count_limit]
+        )
+        return (
+            Scrape.objects.all()
+            .order_by("-created_at")
+            .prefetch_related(
+                Prefetch(
+                    "words",
+                    queryset=WordCount.objects.filter(id__in=subqry).order_by("-count"),
+                )
+            )
+        )
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
