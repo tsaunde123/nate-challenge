94146f7cf4ba3caa48aaf6ebc3af6f5452442c82
TristanSaunders <tristansaundersfr@gmail.com>
2021-08-02 15:40:37 +0100

    fix docker file and docker compose for frontend

--- backend/settings.py
+++ backend/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2ggha+=i4ybg@a2o)^y$uqg2-1@1xg785b3i5_zfj@mj7fdawl'
+SECRET_KEY = "django-insecure-2ggha+=i4ybg@a2o)^y$uqg2-1@1xg785b3i5_zfj@mj7fdawl"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
