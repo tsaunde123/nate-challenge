47bff627164d7850bb47d858f50760eab5f4e7da
TristanSaunders <tristansaundersfr@gmail.com>
2021-08-02 03:15:48 +0100

    backend: rename view

--- backend/core/api.py
+++ backend/core/api.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from scraper.views import time, scrape, history
+from scraper.views import time, scrape_view, history
 
 urlpatterns = [
     path("time", time),
-    path("scrape", scrape),
+    path("scrape", scrape_view),
     path("history", history),
 ]
