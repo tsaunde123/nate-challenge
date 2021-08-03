50f6347a5082176103b85ca10687b6d123ab2cce
TristanSaunders <tristansaundersfr@gmail.com>
2021-08-03 11:22:44 +0100

    front: extract components in details view. Add select to sort and include in API request to fetch details

--- frontend/components/detail/index.tsx
+++ frontend/components/detail/index.tsx
@@ -1,12 +1,93 @@
 import { useRouter } from "next/router";
-import { useScrape } from "./useScrape";
+import { Sort, useScrape } from "./useScrape";
 import cx from "classnames";
+import { IScrape } from "@/types/scrape";
+import { useState } from "react";
+
+function Status(scrape: IScrape) {
+  return (
+    <span
+      className={cx(
+        "px-2 inline-flex text-xs leading-5 font-semibold rounded-full mb-1",
+        {
+          "bg-green-100 text-green-800": !!scrape.completed_at && !scrape.error,
+          "bg-yellow-100 text-yellow-800":
+            !scrape.completed_at && !scrape.error,
+          "bg-red-100 text-red-800": scrape.error,
+        }
+      )}
+    >
+      {scrape.error ? "Error" : scrape.completed_at ? "Succeeded" : "Pending"}
+    </span>
+  );
+}
+
+function Words(scrape: IScrape) {
+  return (
+    <div className="flex flex-col">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block  sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Word
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Count
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {((scrape.words as []) || []).map((wordcount: any, id) => (
+                  <tr key={id}>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="flex items-center">
+                        <div className="ml-4">
+                          <div className="text-sm font-medium text-gray-900">
+                            {wordcount.word}
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <span
+                        className={cx(
+                          "px-2 inline-flex text-xs leading-5 font-semibold rounded-full"
+                        )}
+                      >
+                        {wordcount.count}
+                      </span>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default function DetailView() {
   const {
     query: { id },
   } = useRouter();
-  const { scrape, isLoading, isError } = useScrape(id);
+  const [sortFn, setSortFn] = useState(Sort.CountDESC);
+  const { scrape, isLoading, isError } = useScrape(id, sortFn);
+
+  function changeSortFn(e) {
+    e.preventDefault();
+    setSortFn(e.target.value);
+  }
 
   return (
     <div className="mt-4">
@@ -18,76 +99,24 @@ export default function DetailView() {
       ) : (
         <>
           <h2 className="text-xl text-gray-700 mb-1">{`${scrape.url}: `}</h2>
-          <span
-            className={cx(
-              "px-2 inline-flex text-xs leading-5 font-semibold rounded-full mb-1",
-              {
-                "bg-green-100 text-green-800":
-                  !!scrape.completed_at && !scrape.error,
-                "bg-yellow-100 text-yellow-800":
-                  !scrape.completed_at && !scrape.error,
-                "bg-red-100 text-red-800": scrape.error,
-              }
-            )}
-          >
-            {scrape.error
-              ? "Error"
-              : scrape.completed_at
-              ? "Succeeded"
-              : "Pending"}
-          </span>
-          <div className="flex flex-col">
-            <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-              <div className="py-2 align-middle inline-block  sm:px-6 lg:px-8">
-                <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                  <table className="min-w-full divide-y divide-gray-200">
-                    <thead className="bg-gray-50">
-                      <tr>
-                        <th
-                          scope="col"
-                          className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                        >
-                          Word
-                        </th>
-                        <th
-                          scope="col"
-                          className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                        >
-                          Count
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody className="bg-white divide-y divide-gray-200">
-                      {((scrape.words as []) || []).map(
-                        (wordcount: any, id) => (
-                          <tr key={id}>
-                            <td className="px-6 py-4 whitespace-nowrap">
-                              <div className="flex items-center">
-                                <div className="ml-4">
-                                  <div className="text-sm font-medium text-gray-900">
-                                    {wordcount.word}
-                                  </div>
-                                </div>
-                              </div>
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap">
-                              <span
-                                className={cx(
-                                  "px-2 inline-flex text-xs leading-5 font-semibold rounded-full"
-                                )}
-                              >
-                                {wordcount.count}
-                              </span>
-                            </td>
-                          </tr>
-                        )
-                      )}
-                    </tbody>
-                  </table>
-                </div>
-              </div>
+          <div className="flex items-center space-x-10 mb-2">
+            <div>
+              <Status {...scrape} />
             </div>
+            <select
+              name="sort"
+              id="sort"
+              className="text-sm mt-1 block rounded-md"
+              value={sortFn}
+              onChange={changeSortFn}
+            >
+              <option value={Sort.WordASC}>Word ASC</option>
+              <option value={Sort.WordDESC}>Word DESC</option>
+              <option value={Sort.CountASC}>Count ASC</option>
+              <option value={Sort.CountDESC}>Count DESC</option>
+            </select>
           </div>
+          <Words {...scrape} />
           {!scrape.completed_at && <p className="mt-1">Processing...</p>}
         </>
       )}
--- frontend/components/detail/useScrape.ts
+++ frontend/components/detail/useScrape.ts
@@ -3,16 +3,28 @@ import fetcher from "@/lib/fetcher";
 import { IScrape } from "@/types/scrape";
 import useSWR from "swr";
 
+// TODO get these options from the backend rather than hard-coding
+export enum Sort {
+  WordASC = "word-asc",
+  WordDESC = "word-desc",
+  CountASC = "count-asc",
+  CountDESC = "count-desc",
+}
+
 interface IScrapeResponse {
   scrape?: IScrape;
   isLoading: boolean;
   isError: any;
 }
 
-export function useScrape(id): IScrapeResponse {
-  const { data, error } = useSWR(id && `${API_URL}/scrapes/${id}`, fetcher, {
-    refreshInterval: 5000,
-  }); // refresh every 5 seconds
+export function useScrape(id: number, sortFn: Sort): IScrapeResponse {
+  const { data, error } = useSWR(
+    id && `${API_URL}/scrapes/${id}?sort=${sortFn}`,
+    fetcher,
+    {
+      refreshInterval: 5000,
+    }
+  ); // refresh every 5 seconds
 
   return {
     scrape: data as IScrape | null,
